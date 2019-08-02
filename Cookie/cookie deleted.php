5ff00c4958a137f8f014d092ee96038f885210cb
mustafa savun <drwebmastersm@gmail.com>
2019-08-02 16:04:18 +0300

    Create cookie deleted.php

---
+++ Cookie/cookie deleted.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set("Europe/istanbul");
+
+$yazi="merhaba dünya";
+setcookie("isim",$yazi, strtotime("+1 week"));
+
+echo $_COOKIE["isim"];
+setcookie("isim","",strtotime("-1 week"));
+
+?>  
